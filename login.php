f29eb0cb9d5de2896c49f6226cf7eefc2fc4d5a5
Ferovax <ferovax@gmail.com>
2018-01-30 16:18:44 +0000

    fixed mvc

--- login.php
+++ login.php
@@ -1,4 +1,31 @@
 <?php
+require_once ('models/connectdb.php');
+require_once ('models/User.php');
+$user = new User($connection);
+if($user->is_loggedin()!="")
+{
+    $user->redirect('index.php');
+}
+if(isset($_POST['btn-login']))
+{
+    $username = $_POST['username'];
+    $email = $_POST['username'];
+    $pass = $_POST['password'];
+    $userMail = $_POST['email'];
+
+
+    if($user->login($username,$email,$pass))
+    {
+        session_start();
+        $_SESSION['username'] = $username;
+        $_SESSION['email'] = $userMail;
+        $user->redirect('index.php');
+    }
+    else
+    {
+        $error = "Wrong Details !";
+    }
+}
 
 $view = new stdClass();
 $view->pageTitle = 'Login';
