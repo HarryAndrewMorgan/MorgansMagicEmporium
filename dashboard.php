6784707fbf97fdff04a7fb0202516af6224ddd3b
Ferovax <ferovax@gmail.com>
2018-01-31 15:32:03 +0000

    Fixed dashboard session variables

--- dashboard.php
+++ dashboard.php
@@ -3,13 +3,13 @@ require_once ('models/connectdb.php');
 require_once ('models/User.php');
 $user = new User($connection);
 session_start();
-if(!isset($_SESSION['username']))
+if(!isset($_SESSION['Username']))
 {
     $user->redirect('index.php');
     exit;
 }
-$username = $_SESSION['username'];
-$email = $_SESSION['email'];
+$username = $_SESSION['Username'];
+$email = $_SESSION['Email'];
 $sqlQuery = $connection->prepare("SELECT * FROM Users WHERE Username=".$username);
 $sqlQuery->execute(array(":user"=>$username));
 $userRow=$sqlQuery->fetch(PDO::FETCH_ASSOC);
