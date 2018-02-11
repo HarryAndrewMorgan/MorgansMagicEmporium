1c452bd07a2035142993d468db81083e2436a397
Ferovax <ferovax@gmail.com>
2018-02-11 04:44:38 +0000

    fixed empty filter

--- index.php
+++ index.php
@@ -6,17 +6,23 @@ require_once ('models/Advert.php');
 $_dbHandle = Database::getInstance()->getdbConnection();
 $advert = new Advert($_dbHandle);
 $view->numberOfRows = $advert->countAdverts();
-$filter = $_GET['filter'];
-if ($filter == 1){
-    $type = "Singles";
-    $_SESSION['filterName'] = "Singles";
-} elseif ($filter == 2) {
-    $type = "Boosters";
-    $_SESSION['filterName'] = "Boosters";
-} elseif ($filter == 3) {
-    $type="Accessories";
-    $_SESSION['filterName'] = "Accessories";
+
+$type="Singles";
+if(isset($_GET['filter']))
+{
+    $filter = $_GET['filter'];
+    if ($filter == 1){
+        $type = "Singles";
+        $_SESSION['filterName'] = "Singles";
+    } elseif ($filter == 2) {
+        $type = "Boosters";
+        $_SESSION['filterName'] = "Boosters";
+    } elseif ($filter == 3) {
+        $type="Accessories";
+        $_SESSION['filterName'] = "Accessories";
+    }
 }
+
 $view->adverts = $advert->filterAdverts($type);
 $view->pageTitle = 'Homepage';
 require_once('Views/index.phtml');
