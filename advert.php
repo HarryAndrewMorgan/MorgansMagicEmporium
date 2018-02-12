f456b762eb5f9b9a484f5754903410baec59fe36
Ferovax <ferovax@gmail.com>
2018-02-12 18:42:55 +0000

    Half way through adding saved adverts cant get button responding

--- advert.php
+++ advert.php
@@ -7,9 +7,14 @@ $_dbHandle = Database::getInstance()->getdbConnection();
 $user = new User($_dbHandle);
 $advert = new Advert($_dbHandle);
 $view = new stdClass();
+if(isset($_POST['save']))
+{
+    $advert->saveAdvert($_SESSION['UserID'], $_GET['id']);
+}
 if(isset($_GET['id']))
 {
     $view->adverts = $advert->returnAdvert($_GET['id']);
 }
+
 $view->pageTitle = 'Advert';
 require_once('views/advert.phtml');
